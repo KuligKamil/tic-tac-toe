8df0cf3c00ae35bf9a3ce9d9a3e60929f524d594
KuligKamil <kamil.marek.kulig@gmail.com>
2021-01-30 23:21:03 +0100

    #3 fix: typo, add args for mypy for founding errors

--- engine.py
+++ engine.py
@@ -1,16 +1,17 @@
 from typing import List
 
 
-def show_board(char):
-    board: List = [char for i in range(9)]
-    graphic = f"""
+def show_board(char: str) -> None:
+    board: List = [char for _ in range(9)]
+    print(
+        f"""
        {board[0]} ┃ {board[1]} ┃ {board[2]}
      ━━━━╋━━━╋━━━━
        {board[3]} ┃ {board[4]} ┃ {board[5]}
      ━━━━╋━━━╋━━━━
        {board[6]} ┃ {board[7]} ┃ {board[8]}
     """
-    print(graphic)
+    )
 
 
 if __name__ == "__main__":
